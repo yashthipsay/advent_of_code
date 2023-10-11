6cefda00f2040acd7cc78f84c60d1f5071a3b1ce
Yash Thipsay <yashthpsay@gmail.com>
2023-10-11 11:10:13 +0530

    struct and tuple

--- rust_lang/main.rs
+++ rust_lang/main.rs
@@ -1,5 +1,15 @@
 fn main(){
     let a = "Rust";
+    let mut number = 10;
     println!("Hello, World!");
     println!("Programming - {}", a);
+
+    let number2: u32 =  15;
+
+    struct Student {
+        name: String,
+        age: u8,
+    }
+    let user_1 = Student{name: String::from("John"), age: 25};
+    let user_2 = Student{name: String::from("Jane"), age: 20};
 }
