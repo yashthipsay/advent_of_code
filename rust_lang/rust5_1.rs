9f8c6da6bc67bbeedb1aea4820888bbc3398b3f7
yashthipsay <yashthpsay@gmail.com>
2023-12-17 21:44:58 +0530

    rust lang

--- rust_lang/rust5_1.rs
+++ rust_lang/rust5_1.rs
@@ -1,15 +1,35 @@
+// fn main() {
+//     let mut greeting2 = String::from("Greeting");
+//     print_greeting(&mut greeting2);
+//     let greeting = String::from("Hello");
+//     print_greeting(&greeting);
+//     print_greeting(&greeting);
+// }
+
+// fn print_greeting(message: &String){
+//     println!("Greeting: {}", message);
+// }
+
+// fn print_greeting2(message: &mut String){
+//     message.push_str("");
+// }
+
+
+fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("x is {} and y is {}", x, y);
+}
+
+// Error message:
+// fn failed_borrow<'a>() {
+//     let _a = 12;
+
+//     let y: &'a i32 = &_x;
+// }
+
 fn main() {
-    let mut greeting2 = String::from("Greeting");
-    print_greeting(&mut greeting2);
-    let greeting = String::from("Hello");
-    print_greeting(&greeting);
-    print_greeting(&greeting);
-}
+    let (four, nine) = (4, 9);
 
-fn print_greeting(message: &String){
-    println!("Greeting: {}", message);
-}
+    print_refs(&four, &nine);
 
-fn print_greeting2(message: &mut String){
-    message.push_str("");
+    
 }
--- rust_lang/rust5_2.rs
+++ rust_lang/rust5_2.rs
@@ -1,21 +1,51 @@
-fn copy_and_return<'a>(vector: &'a mut Vec<String>, value: &'a str) -> &'a String {
-    vector.push(String::from(value));
-    vector.get(vector.len() - 1).unwrap()
+// fn copy_and_return<'a>(vector: &'a mut Vec<String>, value: &'a str) -> &'a String {
+//     vector.push(String::from(value));
+//     vector.get(vector.len() - 1).unwrap()
+// }
+
+// fn main() {
+//     let name1 = "Joe";
+//     let name2 = "Chris";
+//     let name3 = "Anne";
+
+//     let mut names = Vec::new();
+
+//     assert_eq!("Joe", copy_and_return(&mut names, &name1));
+//     assert_eq!("Chris", copy_and_return(&mut names, &name2));
+//     assert_eq!("Anne", copy_and_return(&mut names, &name3));
+
+//     assert_eq!(
+//         names,
+//         vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
+//     )
+// }
+
+
+fn print_one<'a>(x: &'a i32) {
+    println!("`print_one`: x is {}", x);
 }
 
+fn add_one<'a>(x: &'a mut i32) {
+    *x += 1;
+}
+
+fn print_multi<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("`print_multi`: x is {}, y is {}", x, y);
+
+}
+
+fn pass_x<'a, 'b>(x: &'a i32, _: &'b i32) -> &'a i32 {x};
+
 fn main() {
-    let name1 = "Joe";
-    let name2 = "Chris";
-    let name3 = "Anne";
+    let x = 7;
+    let y = 9;
 
-    let mut names = Vec::new();
+    print_one(&x);
+    print_multi(&x, &y);
 
-    assert_eq!("Joe", copy_and_return(&mut names, &name1));
-    assert_eq!("Chris", copy_and_return(&mut names, &name2));
-    assert_eq!("Anne", copy_and_return(&mut names, &name3));
+    let z = pass_x(&x, &y);
 
-    assert_eq!(
-        names,
-        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
-    )
+    let mut t = 3;
+    add_one(&mut t);
+    print_one(&t);
 }
