0eed607f5ac9a4f435b135e9735a7b6827bb2214
yashthipsay <yashthpsay@gmail.com>
2023-12-26 14:56:08 +0530

    cli terminal project

--- cli_terminal/src/main.rs
+++ cli_terminal/src/main.rs
@@ -1,19 +1,35 @@
 use std::env;
 use std::fs;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query  = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = parse_config(&args);
 
-    println!("In file {}", file_path);
-    let contents = fs::read_to_string(file_path)
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
-    println!("With text: \n{contents}");
+    // --snip--
+}
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {query, file_path}
+    }
 }
