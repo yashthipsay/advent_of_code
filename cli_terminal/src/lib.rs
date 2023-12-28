b29b80da1a74b0fa02ddc949441c26e54181d363
yashthipsay <yashthpsay@gmail.com>
2023-12-28 22:37:06 +0530

    cli terminal project

--- cli_terminal/src/lib.rs
+++ cli_terminal/src/lib.rs
@@ -4,6 +4,7 @@ use std::fs;
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -22,13 +23,40 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text: \n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+            search(&config.query, &contents)
+         };
+    
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -37,12 +65,25 @@ mod tests {
 
     #[test]
     fn one_result() {
-        let query = "duct";
+        let query = "Rust.";
         let contents = "\
-        Rust
+        Rust.
         Rustlings
         Rust by example";
 
-        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+        assert_eq!(vec!["Rust."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RuSt tutorial.";
+        let contents = "\
+        Rust tutorial.
+        Rust language";
+
+        assert_eq!(
+            vec!["Rust tutorial."],
+            search_case_insensitive(query, contents)
+        );
+        }
 }
