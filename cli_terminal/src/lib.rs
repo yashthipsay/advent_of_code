449cb840f8925547e6a99ac9e3540ed658d6d0f1
yashthipsay <yashthpsay@gmail.com>
2023-12-29 08:58:58 +0530

    cli terminal project

--- cli_terminal/src/lib.rs
+++ cli_terminal/src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
@@ -16,7 +17,9 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config {query, file_path})
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {query, file_path, ignore_case,})
     }
 }
 
