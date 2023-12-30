2650dd8a993f3a341a616b9e44fe666a219c1ee9
yashthipsay <yashthpsay@gmail.com>
2023-12-30 12:34:15 +0530

    advent od code 1

--- advent_of_code/numerical_1/target/debug/deps/numerical_1-8c67d20301b83d7e.d
+++
@@ -1,5 +0,0 @@
-/root/MEDBLOCKS_BLOCKCHAIN_TEST/advent_of_code/numerical_1/target/debug/deps/numerical_1-8c67d20301b83d7e: src/main.rs
-
-/root/MEDBLOCKS_BLOCKCHAIN_TEST/advent_of_code/numerical_1/target/debug/deps/numerical_1-8c67d20301b83d7e.d: src/main.rs
-
-src/main.rs:
--- advent_of_code/numerical_1/target/debug/numerical_1.d
+++
@@ -1,1 +0,0 @@
-/root/MEDBLOCKS_BLOCKCHAIN_TEST/advent_of_code/numerical_1/target/debug/numerical_1: /root/MEDBLOCKS_BLOCKCHAIN_TEST/advent_of_code/numerical_1/src/main.rs
