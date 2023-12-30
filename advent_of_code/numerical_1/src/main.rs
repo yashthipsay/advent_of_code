915180787e5f558679f3189dd25b8cda769e8347
yashthipsay <yashthpsay@gmail.com>
2023-12-30 12:28:05 +0530

    advent od code 1

--- advent_of_code/numerical_1/src/main.rs
+++ advent_of_code/numerical_1/src/main.rs
@@ -1,11 +1,9 @@
 use std::fmt::Error;
+use std::env;
 
 
 fn main() -> () {
-    let input = vec!["1abc2",
-    "pqr3stu8vwx",
-    "a1b2c3d4e5f",
-    "treb7uchet"];
+    let input: Vec<String> = env::args().collect();
 
     let mut sum: i32 = 0;
 
